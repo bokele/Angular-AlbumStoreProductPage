c9b1f00a703b344e55fc76aa4919893e7116e8fd
bokele wakiza franck <bwakiza@gmail.com>
2020-09-26 17:36:30 +0200

    check 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -12,6 +12,7 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
+    console.log(this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);)
     this._productService.getAlbum(1).subscribe(response=>this.albumInfo = response);
 
 }

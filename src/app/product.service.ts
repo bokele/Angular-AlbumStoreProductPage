49f8009c9884bdc972653247e5da38d6c4ed2d5a
bokele wakiza franck <bwakiza@gmail.com>
2020-09-26 16:32:22 +0200

    ckeck 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
-import map from "rxjs/add/operator/map";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class ProductService {
   private _albumUrl: string = "../assets/album.json";
   constructor(private _http: Http) {}
 
-  function getAlbum(id: number) :number {
-    return this._http.get(this._albumUrl).map((response ) => {
+  getAlbum(id: number): number {
+    return this._http.get(this._albumUrl).map((response) => {
       return response.json();
     });
   }
